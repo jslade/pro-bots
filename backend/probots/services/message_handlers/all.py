71b012f0ffe4b610ebdbb4924a9075c9ccbb6d52
Jeremy Slade <jeremy@jkslade.net>
2025-02-08 01:40:38 -0700

    three.js

--- backend/probots/services/message_handlers/all.py
+++ backend/probots/services/message_handlers/all.py
@@ -1,7 +1,9 @@
 from .connection_handler import ConnectionHandler
+from .manual_control_handler import ManualControlHandler
 from .terminal_handler import TerminalHandler
 
 MESSAGE_HANDLERS = [
     ConnectionHandler(),
     TerminalHandler(),
+    ManualControlHandler(),
 ]
