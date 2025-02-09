c064806bb5a7b0d13036127d8dda96d15655420a
Jeremy Slade <jeremy@jkslade.net>
2025-02-09 00:34:36 -0700

    startup forking fix

--- backend/startup.py
+++ backend/startup.py
@@ -1,20 +1,38 @@
+from datetime import datetime, timedelta
+
 import probots.controllers  # noqa: F401
 import probots.db
 import probots.models.all  # noqa: F401
-from probots.app import APP, SOCK
+from probots.app import APP
 from probots.scheduled_tasks import SCHEDULER
 from probots.services.dispatcher import DISPATCHER
 from probots.services.message_handlers.all import MESSAGE_HANDLERS
 from probots.services.monitor import MONITOR
 
-# Setup the various dispatch handlers
-for handler in MESSAGE_HANDLERS:
-    handler.register(DISPATCHER)
+DEBUG = True
 
-# Initialize the game monitor that oversees the whole operation
-# This ends up starting a background thread that runs separately
-# from the flask stuff happening in the main thread
-MONITOR.run()
+
+def do_setup() -> None:
+    # Setup the various dispatch handlers
+    for handler in MESSAGE_HANDLERS:
+        handler.register(DISPATCHER)
+
+    # Initialize the game monitor that oversees the whole operation
+    # This ends up starting a background thread that runs separately
+    # from the flask stuff happening in the main thread
+    MONITOR.run()
+
+
+# Flask debug causes a process fork, and it takes a bit to happen.
+# If we call the startup task immediately, it's duplicated in the forked
+# process. But doing it as a deferred task ensures it happens post-fork
+
+SCHEDULER.add_job(
+    id="post_fork_setup",
+    func=do_setup,
+    trigger="date",
+    run_date=datetime.now() + timedelta(seconds=1),
+)
 
 if __name__ == "__main__":
     SCHEDULER.start()
