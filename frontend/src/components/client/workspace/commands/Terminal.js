43293d7155c97609b714d140ed03ffb5155ef448
Jeremy Slade <jeremy@jkslade.net>
2025-02-12 19:15:34 -0700

    terminal stuff

--- frontend/src/components/client/workspace/commands/Terminal.js
+++ frontend/src/components/client/workspace/commands/Terminal.js
@@ -8,26 +8,65 @@ const TerminalComponent = () => {
     const api = useContext(ApiContext);
     const [terminalLineData, setTerminalLineData] = useState([]);
 
-    const onInput = useCallback((terminalInput) => {
-        if (terminalInput === 'clear') {
+    const [input, setInput] = useState('');
+    const [history, setHistory] = useState([]);
+    const [historyIndex, setHistoryIndex] = useState(-1);
+  
+    const handleCommand = (command) => {
+        // Process the command
+        if (command === 'clear') {
             setTerminalLineData([]);
-        } else if (terminalInput === '') {
+        } else if (command === '') {
         } else {
             // Show the input line
             setTerminalLineData([
                 ...terminalLineData,
-                <TerminalOutput key={terminalInput.length}>
-                &gt;&gt;&gt;&nbsp;{terminalInput}
+                <TerminalOutput key={terminalLineData.length}>
+                &gt;&gt;&gt;&nbsp;{command}
                 </TerminalOutput>,
             ])
 
-            // Send the input to the server
-            api.sendMessage('terminal', 'input', {
-                input: terminalInput,
-            });
+            sendCommand(command);
+            addToHistory(command)
         }
-    }, [api, terminalLineData])
+    };
+  
+    const sendCommand = useCallback((command) => {
+        // Send the input to the server
+        api.sendMessage('terminal', 'input', {
+            input: command,
+        });
+    }, [api]);
 
+    const addToHistory = useCallback((command) => {
+        // Add to the history -- if it's not the same as the last command
+        if (history.length > 0 && command != history[-1]) {
+            setHistory([...history, command]);
+        }
+        setHistoryIndex(-1); // Reset to -1 to indicate we're back at the current input
+        setInput(''); // Clear the input after command execution
+    }, [history, setHistory, setHistoryIndex, setInput])
+
+    const handleKeyDown = (event) => {
+        console.log(event.key);
+        if (event.key === 'ArrowUp') {
+            event.preventDefault();
+            let newIndex = historyIndex + 1;
+            if (newIndex < history.length) {
+                setInput(history[history.length - 1 - newIndex]);
+                setHistoryIndex(newIndex);
+            }
+        }
+        if (event.key === 'ArrowDown') {
+            event.preventDefault();
+            let newIndex = historyIndex - 1;
+            if (newIndex >= 0) {
+                setInput(history[history.length - 1 - newIndex]);
+                setHistoryIndex(newIndex);
+            }
+        }
+    };
+  
     const onOutput = useCallback((output) => {
         setTerminalLineData([
             ...terminalLineData,
@@ -49,12 +88,13 @@ const TerminalComponent = () => {
     return (
         <div className="container">
         <Terminal
-            name=""
+            name="Command Terminal"
             colorMode={ColorMode.Dark}
             TopButtonsPanel={()=> null}
             height="20em"
             prompt=">>>"
-            onInput={onInput}
+            onInput={handleCommand}
+            startingInputValue={input}
         >
             {terminalLineData}
         </Terminal>
